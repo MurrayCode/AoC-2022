cbc1e859cdd770e7b8c2f9de26266de43bf69e61
MurrayCode <Murraystewart94@hotmail.co.uk>
2022-12-04 13:40:51 +0000

    day4: added tests for part 2 and helper func

--- day4/day4_test.go
+++ day4/day4_test.go
@@ -1,6 +1,9 @@
 package day4
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestPart1(t *testing.T) {
 	input := `2-4,6-8
@@ -17,6 +20,21 @@ func TestPart1(t *testing.T) {
 		t.Errorf("expected %d, got %d", exp, got)
 	}
 }
+func TestPart2(t *testing.T) {
+	input := `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`
+
+	exp := 4
+	got := Part2(input)
+
+	if exp != got {
+		t.Errorf("expected %d, got %d", exp, got)
+	}
+}
 
 func TestContains(t *testing.T) {
 
@@ -24,6 +42,14 @@ func TestContains(t *testing.T) {
 	exp := true
 
 	if got != exp {
+		t.Errorf("expected %v, got %v", exp, got)
+	}
+}
+
+func TestMakeRange(t *testing.T) {
+	got := makeRange(0, 3)
+	exp := []int{0, 1, 2, 3}
+	if !reflect.DeepEqual(exp, got) {
 		t.Errorf("expected %v, got %v", exp, got)
 	}
 }
