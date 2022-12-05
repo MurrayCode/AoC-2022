28fa10e3e79092a677feff6f62dc1b5909831681
MurrayCode <Murraystewart94@hotmail.co.uk>
2022-12-05 21:11:25 +0000

    day5: solved part 2

--- day5/day5.go
+++ day5/day5.go
@@ -11,14 +11,14 @@ type Stack struct {
 	items []rune
 }
 
-func (s *Stack) Push(item rune) []rune {
-	s.items = append(s.items, item)
+func (s *Stack) Push(item []rune) []rune {
+	s.items = append(s.items, item...)
 	return s.items
 }
 
-func (s *Stack) Pop() (r rune) {
-	r = s.items[0]
-	s.items = s.items[1:len(s.items)]
+func (s *Stack) Pop(amount int) (r []rune) {
+	r = s.items[len(s.items)-amount : len(s.items)]
+	s.items = s.items[:len(s.items)-amount]
 	return
 }
 
@@ -40,7 +40,7 @@ func PopulateStacks(input string) [9]Stack {
 			iter++
 			if item[i] != ' ' {
 
-				stacks[iter].Push(rune(item[i]))
+				stacks[iter].addToBottom(rune(item[i]))
 			}
 			if iter == 8 {
 				iter = -1
@@ -50,8 +50,32 @@ func PopulateStacks(input string) [9]Stack {
 	return stacks
 }
 
-func Part1(input string) string {
+// func Part1(input string) string {
+// 	stacks := PopulateStacks(input)
+// 	instructions := strings.Split(input, "\n\n")[1]
+// 	lines := strings.Split(instructions, "\n")
+// 	for _, line := range lines {
+// 		line = regexp.MustCompile(`[^0-9]+`).ReplaceAllString(line, " ")
+// 		nums := strings.Split(line, " ")
+// 		amount, _ := strconv.Atoi(nums[1])
+// 		from, _ := strconv.Atoi(nums[2])
+// 		to, _ := strconv.Atoi(nums[3])
+// 		for i := 0; i < amount; i++ {
+// 			moving := stacks[from-1].Pop()
+// 			stacks[to-1].addToBottom(moving)
+// 		}
+// 	}
+// 	var res string
+// 	for _, stack := range stacks {
+// 		res += fmt.Sprintf("%c", stack.items[0])
+
+// 	}
+// 	return res
+// }
+
+func Part2(input string) string {
 	stacks := PopulateStacks(input)
+	fmt.Println(stacks)
 	instructions := strings.Split(input, "\n\n")[1]
 	lines := strings.Split(instructions, "\n")
 	for _, line := range lines {
@@ -60,15 +84,14 @@ func Part1(input string) string {
 		amount, _ := strconv.Atoi(nums[1])
 		from, _ := strconv.Atoi(nums[2])
 		to, _ := strconv.Atoi(nums[3])
-		for i := 0; i < amount; i++ {
-			moving := stacks[from-1].Pop()
-			stacks[to-1].addToBottom(moving)
-		}
+		moving := stacks[from-1].Pop(amount)
+		stacks[to-1].Push(moving)
 	}
 	var res string
 	for _, stack := range stacks {
-		res += fmt.Sprintf("%c", stack.items[0])
+		res += fmt.Sprintf("%c", stack.Pop(1))
 
 	}
+	res = regexp.MustCompile(`[^A-Z0-9]+`).ReplaceAllString(res, "")
 	return res
 }
--- day5/day5_test.go
+++ day5/day5_test.go
@@ -2,7 +2,26 @@ package day5
 
 import "testing"
 
-func TestPart1(t *testing.T) {
+// func TestPart1(t *testing.T) {
+// 	input := `    [D]
+// [N] [C]
+// [Z] [M] [P]
+//  1   2   3
+
+// move 1 from 2 to 1
+// move 3 from 1 to 3
+// move 2 from 2 to 1
+// move 1 from 1 to 2`
+
+// 	got := Part1(input)
+// 	exp := "CMZ"
+
+// 	if got != exp {
+// 		t.Errorf("expected %s, got %s", exp, got)
+// 	}
+// }
+
+func TestPart2(t *testing.T) {
 	input := `    [D]    
 [N] [C]    
 [Z] [M] [P]
@@ -13,8 +32,8 @@ move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2`
 
-	got := Part1(input)
-	exp := "CMZ"
+	got := Part2(input)
+	exp := "MCD"
 
 	if got != exp {
 		t.Errorf("expected %s, got %s", exp, got)
